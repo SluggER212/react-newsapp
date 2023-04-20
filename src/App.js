c4e6b0def3baafd4c57e968edd1d7990f39da4d5
Sourav Dewanji <126152029+SluggER212@users.noreply.github.com>
2023-04-20 10:56:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News';
+import {BrowserRouter as Router,Routes,Route,} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App =()=> {
+  const pageSize = 15;
+ 
+  const [progress, setProgress] = useState(0)
+
+  return (
+      <div>
+        <Router>
+          <Navbar />
+          <LoadingBar
+          height={3}
+            color='#f11946'
+            progress={progress}
+          />
+          <Routes>
+            <Route exact path='/' element={<News setProgress={setProgress}  key='general' pageSize={pageSize} country='in' category='General' />}></Route>
+            <Route exact path='/business' element={<News setProgress={setProgress}  key='business' pageSize={pageSize} country='in' category='Business' />}></Route>
+            <Route exact path='/entertainment' element={<News setProgress={setProgress}  key='entertainment' pageSize={pageSize} country='in' category='Entertainment' />}></Route>
+            <Route exact path='/General' element={<News setProgress={setProgress}  key='General' pageSize={pageSize} country='in' category='General' />}></Route>
+            <Route exact path='/health' element={<News setProgress={setProgress}  key='health' pageSize={pageSize} country='in' category='Health' />}></Route>
+            <Route exact path='/science' element={<News setProgress={setProgress}  key='science' pageSize={pageSize} country='in' category='Science' />}></Route>
+            <Route exact path='/sports' element={<News setProgress={setProgress}  key='sports' pageSize={pageSize} country='in' category='Sports' />}></Route>
+            <Route exact path='/technology' element={<News setProgress={setProgress}  key='technology' pageSize={pageSize} country='in' category='Technology' />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+
+  export default App;
